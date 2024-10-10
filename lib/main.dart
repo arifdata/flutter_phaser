29a5af73c90e741c2ed8abfd75ad825486903a7c
Arif Mulpratama <arifdata7@gmail.com>
2024-10-10 10:41:55 +0700

    force landscape orientation, add network security config

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 final InAppLocalhostServer localhostServer = InAppLocalhostServer(
@@ -11,20 +12,52 @@ void main() async {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          body: SafeArea(
+      home: SafeArea(
         child: InAppWebView(
-          initialUrlRequest: URLRequest(
-            url: WebUri("http://localhost:8080/index.html"),
-          ),
+          initialUrlRequest:
+              URLRequest(url: WebUri("http://localhost:8080/index.html")),
         ),
-      )),
+      ),
+      // home: Scaffold(
+      //   body: SafeArea(
+      //     child: InAppWebView(
+      //       initialUrlRequest: URLRequest(
+      //         url: WebUri("http://localhost:8080/index.html"),
+      //       ),
+      //     ),
+      //   ),
+      // ),
     );
   }
 }
